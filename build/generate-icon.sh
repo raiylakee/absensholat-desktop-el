49b026d2c443a53458c2cb3eb59fb2046b094903
raiylakee <mra.alfikri@gmail.com>
2026-05-21 08:35:57 +0700

    fix issues

--- build/generate-icon.sh
+++ build/generate-icon.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Generate icon.ico dari PNG untuk Windows NSIS installer
+# Generate icon.ico dan icon.png dari PNG untuk Windows dan Linux installer
 # Requires: imagemagick (convert command)
 # Jalankan dari root proyek: ./build/generate-icon.sh
 
@@ -9,4 +9,8 @@ convert "src/assets/applogo/Logo Sholat-01.png" \
   -define icon:auto-resize=256,128,64,48,32,16 \
   build/icon.ico
 
-echo "Icon generated: build/icon.ico"
+convert "src/assets/applogo/Logo Sholat-01.png" \
+  -resize 512x512 \
+  build/icon.png
+
+echo "Icons generated: build/icon.ico, build/icon.png"
